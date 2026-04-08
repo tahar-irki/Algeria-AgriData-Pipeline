00539a64c093ef7ae37a76ac172714be2cb86b00
tahar-irki <taharirki8@gmail.com>
2026-04-08 17:08:53 +0100

    adjusting files

--- src/crop_dataframe.py
+++ src/crop_dataframe.py
@@ -1,13 +1,31 @@
 import kagglehub
 import shutil
 import os
-tmp_path = kagglehub.dataset_download("shankarpriya2913/crop-and-soil-dataset")
-current_dir = os.getcwd()
-files = os.listdir(tmp_path)
-for file_name in files:
+
+
+PROJECT_ROOT = os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__))
+    )
+
+
+DATA_DIR = os.path.join(PROJECT_ROOT, "data")
+
+
+tmp_path = kagglehub.dataset_download(
+    "miadul/precision-agriculture-crop-selection-dataset"
+)
+
+os.makedirs(DATA_DIR, exist_ok=True)
+
+
+for file_name in os.listdir(tmp_path):
     source = os.path.join(tmp_path, file_name)
-    destination = os.path.join(current_dir, file_name)
+    destination = os.path.join(DATA_DIR, file_name)
+
+    if os.path.exists(destination):
+        os.remove(destination)
+
     shutil.move(source, destination)
     print(f"Moved: {file_name}")
 
-print("\nSuccess! Files are now in your root folder.")
+print(f"\nSuccess! Files are now in: {DATA_DIR}")
